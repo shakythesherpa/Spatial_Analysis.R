1c7661e4536fbddb14898844fd696aaec92e6b8a
SSherpa <sherpashaky@gmail.com>
2013-11-21 08:04:31 -0500

    Re-ran entire regression tests and exported plots

--- Lab3.R
+++ Lab3.R
@@ -29,30 +29,36 @@ library(rgdal)
 
 # Load some data
 dublin = readOGR("data/dublin_voters.shp","dublin_voters")
-dub.gal.nb = read.gal(system.file("etc/weights/dublin.gal", package="spdep")[1])
+
+# Check what this does
+IDs = row.names(dublin)
+
+#Regression model
+elec =GenEl2004 ~ Unempl + LowEduc + Age18_24 + Age25_44 + Age45_64 +SC1 + LARent - DiffADD 
 
 # Create neighbors list
-dub.gal.nb2 = poly2nb(dublin)
+dub.gal.nb = poly2nb(dublin)
 
 # Create weights matrix
-dub.listw = nb2listw(dub.gal.nb2, style="W")
+dub.listw = nb2listw(dub.gal.nb, style="W")
 
 par(mar=c(0,0,0,0))
 plot(dublin, dub='lightgrey', border='white')
 plot(dub.listw, coords=coordinates(dublin), add=TRUE)
 
 # Test for spatial autocorrelation
-moran.test(dublin$CRIME, listw=dub.listw, alternative="two.sided")
+moran.test(dublin$GenEl2004, listw=dub.listw, alternative="two.sided")
 
 # Test for local spatial autocorrelation
 dev.off()
-moran.plot(dublin$CRIME, dub.listw, ylab="Spatially lagged CRIME", xlab="CRIME")
-dub.li = localmoran(dublin$CRIME, dub.listw)
+moran.plot(dublin$GenEl2004, dub.listw, ylab="Spatially lagged Election Turnout", xlab="percentage of population in each ED who voted in 2004 election")
+dub.li = localmoran(dublin$GenEl2004, dub.listw)
 dublin$localm = dub.li[,4]
 spplot(dublin, "localm", main="Local Moran's Ii Z-Scores")
 
 # Run OLS Regression
-dub.lm = lm(CRIME ~ INC + HOVAL, data=dublin)
+
+dub.lm = lm(GenEl2004 ~ Unempl + LowEduc + Age18_24 + Age25_44 + Age45_64 +SC1 + LARent - DiffAdd, data=dublin)
 summary(dub.lm)
 
 # Check residuals for spatial dependence
@@ -72,42 +78,47 @@ bptest(dub.lm)
 
 # Fit spatial regression models
 
-# Spatial lag model
-dub.lag = lagsarlm(CRIME ~ INC + HOVAL, data=dublin, listw=dub.listw)
+# Spatial lag model(Spatial Simultaneous Autoregressive lag model estimation)
+dub.lag = lagsarlm(GenEl2004 ~ Unempl + LowEduc + Age18_24 + Age25_44 + Age45_64 +SC1 + LARent - DiffAdd, data=dublin, listw=dub.listw)
 summary(dub.lag)
 
 # Some more diagnostics
-bptest.sarlm(dub.lag)
+bptest.sarlm(dub.lag)#Breusch-Pagan test
 # LM test suggests there is no more spatial autocorrelation in the data
 # BP test indicates remaining heteroskedasticity in the residuals
 #     Most likely due to misspecification
 
-# Spatial error model
-dub.err = errorsarlm(CRIME ~ INC + HOVAL, data=dublin, listw=dub.listw)
+# Spatial error model(Maximum likelihood estimation of spatial simultaneous autoregressive error models of the form:
+
+dub.err = errorsarlm(GenEl2004 ~ Unempl + LowEduc + Age18_24 + Age25_44 + Age45_64 +SC1 + LARent - DiffAdd, data=dublin, listw=dub.listw)
 summary(dub.err)
 bptest.sarlm(dub.err)
 
-##
+
 ## GWR examples (we'll get to this again later)
 ##
 
-dub.lm = lm(CRIME ~ INC + HOVAL, data=dublin)
-dublin$lmres = residuals(dub.lm)
+dub.lm = lm(GenEl2004 ~ Unempl + LowEduc + Age18_24 + Age25_44 + Age45_64 +SC1 + LARent - DiffAdd, data=dublin)
+dublin$lmres = residuals(dub.lm) # Extract Model Residuals
 
-dub.bw = gwr.sel(CRIME ~ INC + HOVAL, data=dublin)
-dub.gauss = gwr(CRIME ~ INC + HOVAL, data=dublin, bandwidth=dub.bw, hatmatrix=TRUE)
+#Crossvalidation of bandwidth for GWR
+dub.bw = gwr.sel(GenEl2004 ~ Unempl + LowEduc + Age18_24 + Age25_44 + Age45_64 +SC1 + LARent - DiffAdd, data=dublin)
+dub.gauss = gwr(GenEl2004 ~ Unempl + LowEduc + Age18_24 + Age25_44 + Age45_64 +SC1 + LARent - DiffAdd, data=dublin, bandwidth=dub.bw, hatmatrix=TRUE)
 
-spplot(dublin, "CRIME", main="Crimes")
-spplot(dublin, "INC", main="Household Income ($1,000s)")
-spplot(dublin, "HOVAL", main="Housing Value ($1,000s)")
+spplot(dublin, "GenEl2004", main="General Election Turnout in 2004")
+spplot(dublin, "Unempl", main="Percentage of the population in each ED who are unemployed")
+spplot(dublin, "LowEduc", main="Percentage of the population in each ED who are with little formal education")
+spplot(dublin, "Age18_24", main="Percentage of the 18-24 year old population in each ED")
+spplot(dublin, "Age25_44", main="Percentage of the 25-44 year old population in each ED")
+spplot(dublin, "Age45_64", main="Percentage of the 45-64 year old population in each ED")
+spplot(dublin, "SC1", main="percentage of the population in each ED who are social class one (high social class")
+#Plot the LM Residuals
 spplot(dublin, "lmres", main="Linear Model Residuals")
 
-spplot(dub.gauss$SDF, "INC", main="Household Income Params")
-spplot(dub.gauss$SDF, "HOVAL", main="Housing Value Params")
+spplot(dub.gauss$SDF, "Unempl", main="Percentage of the population in each ED who are unemployed")
+spplot(dub.gauss$SDF, "LowEduc", main="Percentage of the population in each ED who are with little formal education")
 
-spplot(dub.gauss$SDF, "INC_se", main="Household Income Std. Error")
-spplot(dub.gauss$SDF, "HOVAL_se", main="Housing Value Std. Error")
 res = dub.gauss$SDF
-dublin$gwrres = dublin$CRIME - test$pred
+dublin$gwrres = dublin$GenEl2004 - res$pred
 spplot(dublin, "gwrres", main="GWR Model Residuals")
 
